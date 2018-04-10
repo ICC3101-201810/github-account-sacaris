4deae274a731217dc53cc5301d12cd81e288678f
ING <you@example.com>
2018-04-10 16:57:37 -0300

    diez abril

--- ConsoleApp3/ConsoleApp3/Vehiculo.cs
+++ ConsoleApp3/ConsoleApp3/Vehiculo.cs
@@ -8,5 +8,32 @@ namespace ConsoleApp3
 {
     class Vehiculo
     {
+        string tipo;
+        string patente;
+        string color;
+
+        public Vehiculo(string mitipo, string mipatente, string micolor)
+        {
+            tipo = mitipo;
+            patente = mipatente;
+            color = micolor;
+        }
+    }
+    class camion : Vehiculo
+    {
+        string marca;
+        string modelo;
+        int ano;
+
+        public camion(string mimarca, string mimodelo, int miano)
+        {
+            marca = mimarca;
+            modelo = mimodelo;
+            ano = miano;
+        }
+    }
+    class auto:Vehiculo
+    {
+
     }
 }
